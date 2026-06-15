03e9c8832069ece1204b45d1ba952ccf088653be
Noname Blank <nonameblank007@gmail.com>
2026-06-15 11:09:42 +0530

    src: main: allow all updates
    Signed-off-by: Noname Blank <nonameblank007@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import time
 
+from telegram import Update
 from telegram.ext import ApplicationBuilder
 
 from .cfg import TOKEN
@@ -17,4 +18,4 @@ Handlers.register(app)
 logger.info("Starting Genji...")
 logger.info("Genji Running.")
 logger.info(f"Genji took {(time.time() - _start_time):.2f}s to startup.")
-app.run_polling(drop_pending_updates=True)
+app.run_polling(allowed_updates=Update.ALL_TYPES, drop_pending_updates=True)
