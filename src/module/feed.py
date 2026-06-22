a3d58b859fa693c49d842f5b6bba7ad2c84c7ec4
Noname Blank <nonameblank007@gmail.com>
2026-06-23 00:03:04 +0530

    src: module: add commands listed
    - fetches anime gif to send from api
    
    Signed-off-by: Noname Blank <nonameblank007@gmail.com>

---
+++ src/module/feed.py
@@ -0,0 +1,66 @@
+#
+# Copyright (C) 2026 NonameBlank007
+#
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# Summary: sends a feed gif
+#
+
+import logging
+import random
+
+import httpx
+from telegram import (
+    Update,
+    constants,
+)
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+)
+
+from ..util.logging import logger
+from ..util.module import Module
+
+logger = logging.getLogger(__name__)
+
+
+class FeedModule(Module):
+    @classmethod
+    def setup(cls, app: Application):
+        app.add_handler(CommandHandler("feed", fd))
+
+
+NEKOS_API = "https://nekos.best/api/v2/feed"
+
+
+EXTRACTORS = {
+    NEKOS_API: lambda d: (d["results"][0]["anime_name"], d["results"][0]["url"]),
+}
+
+
+async def fd(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    async with httpx.AsyncClient() as client:
+        try:
+            anime_name, gif = await fetch_fd(client)
+            reply_to = update.effective_message.reply_to_message.message_id if update.effective_message.reply_to_message else None
+            logger.info(f"({update.message.from_user.id}): {update.effective_chat.title} ({update.message.chat_id}) used /feed")
+            await update.message.reply_animation(
+                gif,
+                caption=f"<b>🎬 Anime</b> • <code>{anime_name}</code>",
+                parse_mode=constants.ParseMode.HTML,
+                reply_to_message_id=reply_to,
+            )
+        except Exception:
+            logger.critical("Failed to fetch feed")
+            await update.message.reply_text("Sorry, I couldn't fetch a feed GIF right now. Please try again later")
+
+
+async def fetch_fd(client: httpx.AsyncClient):
+    api = random.choice(list(EXTRACTORS))
+    response = await client.get(api, timeout=10)
+    if response.status_code == 200:
+        data = response.json()
+        return EXTRACTORS[api](data)
+    return ""
---
+++ src/module/peck.py
@@ -0,0 +1,66 @@
+#
+# Copyright (C) 2026 NonameBlank007
+#
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# Summary: sends a peck gif
+#
+
+import logging
+import random
+
+import httpx
+from telegram import (
+    Update,
+    constants,
+)
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+)
+
+from ..util.logging import logger
+from ..util.module import Module
+
+logger = logging.getLogger(__name__)
+
+
+class PeckModule(Module):
+    @classmethod
+    def setup(cls, app: Application):
+        app.add_handler(CommandHandler("peck", pk))
+
+
+NEKOS_API = "https://nekos.best/api/v2/peck"
+
+
+EXTRACTORS = {
+    NEKOS_API: lambda d: (d["results"][0]["anime_name"], d["results"][0]["url"]),
+}
+
+
+async def pk(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    async with httpx.AsyncClient() as client:
+        try:
+            anime_name, gif = await fetch_pk(client)
+            reply_to = update.effective_message.reply_to_message.message_id if update.effective_message.reply_to_message else None
+            logger.info(f"({update.message.from_user.id}): {update.effective_chat.title} ({update.message.chat_id}) used /peck")
+            await update.message.reply_animation(
+                gif,
+                caption=f"<b>🎬 Anime</b> • <code>{anime_name}</code>",
+                parse_mode=constants.ParseMode.HTML,
+                reply_to_message_id=reply_to,
+            )
+        except Exception:
+            logger.critical("Failed to fetch peck")
+            await update.message.reply_text("Sorry, I couldn't fetch a peck GIF right now. Please try again later")
+
+
+async def fetch_pk(client: httpx.AsyncClient):
+    api = random.choice(list(EXTRACTORS))
+    response = await client.get(api, timeout=10)
+    if response.status_code == 200:
+        data = response.json()
+        return EXTRACTORS[api](data)
+    return ""
