ab05908829dada3a58b81c7d7bbb5ea8fc3d204b
Noname Blank <nonameblank007@gmail.com>
2026-06-27 00:23:22 +0530

    genji: {mise, src/cfg}: configure and update env
    Signed-off-by: Noname Blank <nonameblank007@gmail.com>

--- src/cfg.py
+++ src/cfg.py
@@ -16,3 +16,10 @@ except ImportError:
     if not (TOKEN := os.getenv("BOT_TOKEN")):
         logger.critical("Cannot retrive bot token.")
         raise RuntimeError("Cannot get bot token either from cfg file or environment variable.") from None
+
+try:
+    from .api_init import SUDO_USER
+except ImportError:
+    if not (SUDO_USER := os.getenv("MASTER_USER")):
+        logger.critical("Cannot retrive sudo user.")
+        raise RuntimeError("Cannot get sudo user either from cfg file or environment variable.") from None
